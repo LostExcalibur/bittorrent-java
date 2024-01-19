b252696f455fdaed16d89a76e542b2fb6ba8b193
LostExcalibur <romaindebeaucorps@yahoo.fr>
2024-01-19 03:37:22 +0100

    Parse integers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		String command = args[0];
 		if("decode".equals(command)) {
 			String bencodedValue = args[1];
-			String decoded;
+			Object decoded;
 			try {
 				decoded = decodeBencode(bencodedValue);
 			} catch(RuntimeException e) {
@@ -23,7 +23,8 @@ public class Main {
 
 	}
 
-	static String decodeBencode(String bencodedString) {
+	static Object decodeBencode(String bencodedString) {
+		// Strings
 		if (Character.isDigit(bencodedString.charAt(0))) {
 			int firstColonIndex = 0;
 			for(int i = 0; i < bencodedString.length(); i++) { 
@@ -34,8 +35,14 @@ public class Main {
 			}
 			int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
 			return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
+
+		// Integers
+		} else if (bencodedString.charAt(0) == 'i') {
+			int endIndex = bencodedString.indexOf('e');
+
+			return Long.parseLong(bencodedString.substring(1, endIndex));
 		} else {
-			throw new RuntimeException("Only strings are supported at the moment");
+			throw new RuntimeException("Only strings and integers are supported at the moment");
 		}
 	}
 	
